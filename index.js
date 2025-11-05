f30384288c41aa457343bcf93d1aad418451023c
pabvi-ecom <ecommercetothemoon1@gmail.com>
2025-11-05 15:41:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ app.post("/generate", async (req, res) => {
     ffmpeg.setFfmpegPath(ffmpegPath);
     ffmpeg(inputPath)
       .setStartTime(0)
-      .duration(25)
+      .duration(20)
       .audioCodec("libmp3lame")
       .audioBitrate("128k")
       .audioFrequency(44100)
